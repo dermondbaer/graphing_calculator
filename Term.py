eb6a47b8fe95d4959fa7397629dedab035f53ab2
g4rgoil <pascalxmehnert@web.de>
2016-03-09 18:53:46 +0100

    Attribute und Methoden für Term implementiert

--- Term.py
+++ Term.py
@@ -5,5 +5,16 @@
 
 
 class Term(object):
-    def __init__(self):
-        pass
+    def __init__(self, expression, parsed_expression, variable=False):
+        self.__expression = expression
+        self.__parsed_expression = parsed_expression
+        self.__variable = variable
+
+    def get_expression(self):
+        return self.__expression
+
+    def get_parsed_expression(self):
+        return self.__parsed_expression
+
+    def get_variable(self):
+        return self.__variable
