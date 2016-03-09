c751f3c45b58b5199a026f560dad31f50c360174
g4rgoil <pascalxmehnert@web.de>
2016-03-09 16:52:38 +0100

    Wichitge Klassen implementiert

---
+++ Calculator.py
@@ -0,0 +1,9 @@
+# Pascal Mehnert
+# 09.03.2016
+#
+# V 0.1
+
+
+class Calculator(object):
+    def __init__(self):
+        pass
---
+++ Gui.py
@@ -0,0 +1,9 @@
+# Pascal Mehnert
+# 09.03.2016
+# Grafische Oberfläche für den GTR
+# V 0.1
+
+
+class Gui(object):
+    def __init__(self):
+        pass
