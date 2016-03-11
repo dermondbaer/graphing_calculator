f934708b456bf215de212866e7f584f454d9c4b9
Der Mondbär <dermondbaer@users.noreply.github.com>
2016-03-11 08:57:14 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,19 @@
-# Pascal Mehnert
-# 02.03.2016
+# Paul Birke, Pascal Mehnert
+# 11.03.2016
 # 
 # V 0.1
+
+
+from Gui import Gui *
+
+class Application(object):
+  def __init__(self):
+    self.__gui = Gui()
+  
+  def start(self):
+    self.__gui.start()
+
+
+# main
+app = Application()
+app.start()
