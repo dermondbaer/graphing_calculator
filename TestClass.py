2e9c6a56d134c9734f47ff69c4f8e0585f86d218
g4rgoil <pascalxmehnert@web.de>
2016-03-20 22:57:26 +0100

    updated

--- TestClass.py
+++ TestClass.py
@@ -3,7 +3,14 @@
 # Testfunktionen
 # V 0.1
 
+from Parser import Parser
+from ParserTree import ParserTree
+
 
 class TestClass(object):
     def __init__(self):
+        self.__parser = Parser()
+
+    def test_parser(self):
         pass
+
