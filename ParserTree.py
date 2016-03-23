ea293551a183415db7c6bb982e5b5bc677094157
g4rgoil <pascalxmehnert@web.de>
2016-03-23 20:33:07 +0100

    Added documentation for ParserTree.

--- ParserTree.py
+++ ParserTree.py
@@ -5,116 +5,106 @@
 
 
 class Node:
-    def __init__(self, value, is_operation=False, is_value=False, is_constant=False, is_variable=False):
-        """
-        :param value: The value of the Node.
-        :type value: string
-        :param is_operation: Defines whether the Node represents an operation.
-        :type is_operation: Bool
-        :param is_value: Defines whether the Node represents a value.
-        :type is_value: Bool
-        :param is_constant: Defines whether the Node represents a constant.
-        :type is_constant: Bool
-        :param is_variable: Defines whether the Node represents a variable.
-        :type is_variable: Bool
-        :return: The Node that has been created.
-        :rtype: Node
-        """
+    """Represents either an operation, a number, a constant or a variable in a Parser Tree."""
+    def __init__(self, value, is_operation=False, is_number=False, is_constant=False, is_variable=False):
         self.__child_list = []
         self.__value = value
 
-        if is_operation ^ is_value ^ is_constant ^ is_variable:
+        if is_operation ^ is_number ^ is_constant ^ is_variable:
             self.__is_operation = is_operation
-            self.__is_value = is_value
+            self.__is_number = is_number
             self.__is_constant = is_constant
             self.__is_variable = is_variable
 
     def get_child_list(self):
+        """Returns a list, containing all child elements of this Node."""
         return self.__child_list
 
     def get_child(self, index):
+        """Returns a specific child of this Node."""
         return self.__child_list[index]
 
-    def get_child_count(self):
-        return len(self.__child_list)
-
     def get_value(self):
+        """Returns the value of this Node."""
         return self.__value
 
     def is_operation(self):
+        """Determines if this Node represents an operation."""
         return self.__is_operation
 
-    def is_value(self):
-        return self.__is_value
+    def is_number(self):
+        """Determines if this Node represents a number."""
+        return self.__is_number
 
     def is_constant(self):
+        """Determines if this Node represents a constant."""
         return self.__is_constant
 
     def is_variable(self):
+        """Determines if  this Node represents a variable."""
         return self.__is_variable
 
     def add_child(self, child, reverse=False):
-        """
-        Adds an child element to a Node
-
-        :param child: The value to add as a child
-        :type child: Node
-        :param reverse: If True: Adds the child as new left child; Else: Adds the child as new right child
-        :type reverse: Bool
-        :return: The value that was added as child
-        :rtype: Node
-        """
-
+        """Adds a child element to this Node."""
         if reverse is False:
-            self.__child_list.append(child)
+            self.__child_list.append(child)     # adding as most right child
         else:
-            self.__child_list.insert(0, child)
+            self.__child_list.insert(0, child)      # adding as most left child
         return child
 
 
 class ParserTree:
+    """Used to save a mathematical Expression as a Tree."""
     def __init__(self):
         self.__root = None
 
     def get_root(self):
+        """Returns the root Node of this Parser Tree."""
         return self.__root
 
     def add_operation(self, value, reverse=True, parent=None):
+        """Adds an operation as child of a Node."""
         return self.__add_node(value, is_operation=True, reverse=reverse, parent=parent)
 
-    def add_value(self, value, reverse=True, parent=None):
-        return self.__add_node(value, is_value=True, reverse=reverse, parent=parent)
+    def add_number(self, value, reverse=True, parent=None):
+        """Adds a value as child of a Node."""
+        return self.__add_node(value, is_number=True, reverse=reverse, parent=parent)
 
     def add_constant(self, value, reverse=True, parent=None):
+        """Adds a constant as child of a Node."""
         return self.__add_node(value, is_constant=True, reverse=reverse, parent=parent)
 
     def add_variable(self, value, reverse=True, parent=None):
+        """Adds a variable as child of a Node."""
         return self.__add_node(value, is_variable=True, reverse=reverse, parent=parent)
 
-    def __add_node(self, value, is_operation=False, is_value=False, is_constant=False, is_variable=False, reverse=True,
+    def __add_node(self, value, is_operation=False, is_number=False, is_constant=False, is_variable=False, reverse=True,
                    parent=None):
+        """Adds a child to a Node in this Parser Tree."""
         if parent is None:
             if self.__root is None:
-                self.__root = Node(value, is_operation=is_operation, is_value=is_value, is_constant=is_constant,
-                                   is_variable=is_variable)
+                self.__root = Node(value, is_operation=is_operation, is_number=is_number, is_constant=is_constant,
+                                   is_variable=is_variable)     # Adding as root
                 return self.__root
 
         elif parent.is_operation():
-            child = Node(value, is_operation=is_operation, is_value=is_value, is_constant=is_constant,
+            child = Node(value, is_operation=is_operation, is_number=is_number, is_constant=is_constant,
                          is_variable=is_variable)
-            return parent.add_child(child, reverse=reverse)
+            return parent.add_child(child, reverse=reverse)     # Adding as child of an operation
 
         else:
             raise SyntaxError('Error while parsing the expression')
 
     def print(self):
+        """Initializes the print for this Parser Tree."""
         if self.__root is not None:
-            self.__print(self.__root)
+            self.__print(self.__root)   # Calls the __print() function for the root of this Parser Tree
         print()
 
     def __print(self, node):
+        """Prints the given Node and initializes the print for every child element."""
         if node is not None:
             value = node.get_value()
             for child in node.get_child_list():
-                self.__print(child)
+                self.__print(child) # Calls the __print() function for every child element of this Node
             print(value, end=' ')
