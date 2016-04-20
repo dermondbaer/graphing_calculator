12ce83756641be411f80595a2bc9961c07f9b373
g4rgoil <pascalxmehnert@web.de>
2016-04-21 00:14:04 +0200

    Implemented a first version of the math library

--- Math.py
+++ Math.py
@@ -1,44 +1,90 @@
 # Max Trinter, Pascal Mehnert
 # 23.03.2016
 # Collection of mathematical functions
-# V 0.1
+# V 1.0
+
+import math
+
+
+pi = math.pi
+e = math.e
 
 
 def cos(x):
-    pass
+    return math.cos(x)
+
+
+def acos(x):
+    return math.acos(x)
+
+
+def acosh(x):
+    return math.acosh(x)
 
 
 def sin(x):
-    pass
+    return math.sin(x)
+
+
+def asin(x):
+    return math.asin(x)
+
+
+def asinh(x):
+    return math.asinh(x)
 
 
 def tan(x):
-    pass
+    return math.tan(x)
+
+
+def atan(x):
+    return math.atan(x)
+
+
+def atan2(y, x):
+    return math.atan2(y, x)
+
+
+def atanh(x):
+    return math.atanh(x)
+
+
+def radians(x):
+    return math.radians(x)
+
+
+def degrees(x):
+    return math.degrees(x)
 
 
 def sqrt(x):
-    pass
+    return math.sqrt(x)
 
 
-def nth_root(n, x):
-    pass
+def log(x, base):
+    return math.log(x, base=base)
 
 
-def log(x):
-    pass
+def log2(x):
+    return math.log2(x)
+
+
+def log10(x):
+    return math.log10(x)
 
 
 def ln(x):
-    pass
+    return math.log(x, base=math.e)
 
 
-def log_n(n, x):
-    pass
+def factorial(x):
+    return math.factorial(x)
 
 
-def max(x, y):
-    pass
+def max(a, b):
+    return max(a, b)
 
 
-def abs(x, y):
-    pass
+def abs(x):
+    return abs(x)
